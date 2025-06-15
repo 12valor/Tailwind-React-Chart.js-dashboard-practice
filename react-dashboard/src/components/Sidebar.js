6ed1767026de2bd8d8547bb7cfd50a12dbb8f574
GieTUP <evangelista.agdiaz@gmail.com>
2025-06-15 17:42:39 +0800

    ps

--- react-dashboard/src/components/Sidebar.js
+++ react-dashboard/src/components/Sidebar.js
@@ -6,10 +6,10 @@ const Sidebar = () => {
       <h2 className="text-2xl font-bold mb-6">Student Dashboard</h2>
       <nav>
         <ul className="space-y-4">
-          <li className="hover:text-maroon-light cursor-pointer">Overview</li>
-          <li className="hover:text-maroon-light cursor-pointer">Products</li>
-          <li className="hover:text-maroon-light cursor-pointer">Orders</li>
-          <li className="hover:text-maroon-light cursor-pointer">Cashier</li>
+          <li className="bg-red-700 rounded-md hover:text-maroon cursor-pointer shadow-xl">Overview</li>
+          <li className="bg-red-700 rounded-md hover:text-maroon cursor-pointer shadow-xl">Products</li>
+          <li className="bg-red-700 rounded-md hover:text-maroon cursor-pointer shadow-xl">Orders</li>
+          <li className="bg-red-700 rounded-md hover:text-maroon cursor-pointer shadow-xl">Cashier</li>
         </ul>
       </nav>
     </div>
